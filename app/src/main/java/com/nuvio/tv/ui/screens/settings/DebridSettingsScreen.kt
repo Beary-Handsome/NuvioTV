8e9eb7b0620d369e62132cc7d9158cb9dae6bc60
Beary-Handsome <michael.abballe@gmail.com>
2026-05-24 14:44:25 +0000

    Fix exhaustive when for BasicAuth enum branch

--- app/src/main/java/com/nuvio/tv/ui/screens/settings/DebridSettingsScreen.kt
+++ app/src/main/java/com/nuvio/tv/ui/screens/settings/DebridSettingsScreen.kt
@@ -198,6 +198,7 @@ fun DebridSettingsContent(
                                     when (provider.authMethod) {
                                         DebridProviderAuthMethod.DeviceCode -> activeDeviceAuthDialog = provider.id
                                         DebridProviderAuthMethod.ApiKey -> activeApiKeyDialog = provider.id
+                                        DebridProviderAuthMethod.BasicAuth -> activeApiKeyDialog = provider.id
                                     }
                                 },
                                 enabled = true
@@ -1579,6 +1580,7 @@ private fun providerCredentialStatus(
     when (provider.authMethod) {
         DebridProviderAuthMethod.DeviceCode -> if (credential.isBlank()) notSetLabel else connectedLabel
         DebridProviderAuthMethod.ApiKey -> maskDebridApiKey(credential, notSetLabel)
+        DebridProviderAuthMethod.BasicAuth -> if (credential.isBlank()) notSetLabel else connectedLabel
     }
 
 private enum class DebridStreamPicker {
